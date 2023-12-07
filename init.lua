b96caa2aa05efc9553e5771f84e7c6fd6f9617ac
Hengwen Zhang <h.w.zhangvic@gmail.com>
2023-12-07 20:31:28 +0100

    Enable rust-analyzer all features

--- init.lua
+++ init.lua
@@ -34,6 +34,7 @@ return {
             cargo = {
               extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
               extraArgs = { "--profile", "rust-analyzer" },
+              features = "all",
             },
           },
         },
